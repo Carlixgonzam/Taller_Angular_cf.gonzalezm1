a70e39a64bb56ed74d5da3fc8a94ad38b02c19b9
Carla González Mina <158210542+Carlixgonzam@users.noreply.github.com>
2025-10-26 19:22:48 -0500

    CORRECION DE NOMBRES

--- src/app/serie/serie.ts
+++ src/app/serie/serie.ts
@@ -4,16 +4,16 @@ export class Serie {
     channel: string;
     seasons:number;
     description:string;
-    urlpagina:string;
-    urlimagen:string;
-    
-    public constructor(id:number,name:string,channel:string,seasons:number,description:string,urlpagina:string,urlimagen:string){
+    webpage:string;
+    poster:string;
+
+    public constructor(id:number,name:string,channel:string,seasons:number,description:string,webpage:string,poster:string){
         this.id=id;
         this.name=name;
         this.channel=channel;
         this.seasons=seasons;
         this.description=description;
-        this.urlpagina=urlpagina;
-        this.urlimagen=urlimagen;
+        this.webpage=webpage;
+        this.poster=poster;
     }
 }
